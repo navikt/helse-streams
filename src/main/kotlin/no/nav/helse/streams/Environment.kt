5f96eca5027913a95a012f16ee41044c5ddd17dc
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-22 14:32:04 +0100

    Relax stream config a bit, and made username and password optional. Optional username and password lets us easily test against a local kafka cluster

--- src/main/kotlin/no/nav/helse/streams/Environment.kt
+++ src/main/kotlin/no/nav/helse/streams/Environment.kt
@@ -1,13 +1,13 @@
 package no.nav.helse.streams
 
 data class Environment(
-   val username: String = getRequiredEnvVar("KAFKA_PASSWORD"),
-   val password: String = getRequiredEnvVar("KAFKA_PASSWORD"),
-   val bootstrapServersUrl: String = getRequiredEnvVar("KAFKA_BOOTSTRAP_SERVERS"),
-   val schemaRegistryUrl: String? = getEnvVar("KAFKA_SCHEMA_REGISTRY_URL"),
-   val httpPort: Int? = null,
-   val navTruststorePath: String? = getEnvVar("NAV_TRUSTSTORE_PATH"),
-   val navTruststorePassword: String? = getEnvVar("NAV_TRUSTSTORE_PASSWORD")
+val username: String? = getEnvVar("KAFKA_PASSWORD"),
+val password: String? = getEnvVar("KAFKA_PASSWORD"),
+val bootstrapServersUrl: String = getRequiredEnvVar("KAFKA_BOOTSTRAP_SERVERS"),
+val schemaRegistryUrl: String? = getEnvVar("KAFKA_SCHEMA_REGISTRY_URL"),
+val httpPort: Int? = null,
+val navTruststorePath: String? = getEnvVar("NAV_TRUSTSTORE_PATH"),
+val navTruststorePassword: String? = getEnvVar("NAV_TRUSTSTORE_PASSWORD")
    )
 
 private fun getRequiredEnvVar(varName: String) =
--- src/main/kotlin/no/nav/helse/streams/KafkaCredential.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.helse.streams
-
-data class KafkaCredential(val username: String, val password: String) {
-   override fun toString(): String {
-      return "username '$username' password '*******'"
-   }
-}
