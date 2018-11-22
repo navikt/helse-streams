7bf90497961c7f0aa2e88a15373621ca83fdf00c
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-22 14:23:20 +0100

    Made schema registry optional and removed state dir

--- src/main/kotlin/no/nav/helse/streams/Environment.kt
+++ src/main/kotlin/no/nav/helse/streams/Environment.kt
@@ -4,11 +4,10 @@ data class Environment(
    val username: String = getRequiredEnvVar("KAFKA_PASSWORD"),
    val password: String = getRequiredEnvVar("KAFKA_PASSWORD"),
    val bootstrapServersUrl: String = getRequiredEnvVar("KAFKA_BOOTSTRAP_SERVERS"),
-   val schemaRegistryUrl: String = getRequiredEnvVar("KAFKA_SCHEMA_REGISTRY_URL"),
+   val schemaRegistryUrl: String? = getEnvVar("KAFKA_SCHEMA_REGISTRY_URL"),
    val httpPort: Int? = null,
    val navTruststorePath: String? = getEnvVar("NAV_TRUSTSTORE_PATH"),
-   val navTruststorePassword: String? = getEnvVar("NAV_TRUSTSTORE_PASSWORD"),
-   val stateDir: String? = getEnvVar("STATE_DIR")
+   val navTruststorePassword: String? = getEnvVar("NAV_TRUSTSTORE_PASSWORD")
    )
 
 private fun getRequiredEnvVar(varName: String) =
